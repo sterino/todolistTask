7c5cee7038accf75aca2b4263a4b21d434792909
sterino <egore.chip@gmail.com>
2025-02-12 14:16:20 +0500

    feat: domain(dto and entity) added

--- internal/domain/task/dto.go
+++ internal/domain/task/dto.go
@@ -1,1 +1,72 @@
 package task
+
+import (
+	"errors"
+	"net/http"
+	"time"
+)
+
+type Request struct {
+	Title       string    `json:"title" binding:"required"`
+	Description string    `json:"description"`
+	Status      string    `json:"status" binding:"required"`
+	Priority    string    `json:"priority"`
+	StartDate   time.Time `json:"start_date" binding:"required"`
+	EndDate     time.Time `json:"end_date" binding:"required"`
+}
+
+func (s *Request) Bind(r *http.Request) error {
+	if s.Title == "" {
+		return errors.New("title: cannot be blank")
+	}
+	if s.Description == "" {
+		return errors.New("description: cannot be blank")
+	}
+	if s.Status == "" {
+		return errors.New("status: cannot be blank")
+	}
+	if s.Priority == "" {
+		return errors.New("priority: cannot be blank")
+	}
+	if s.StartDate.IsZero() {
+		return errors.New("start_date: cannot be blank")
+	}
+	if s.EndDate.IsZero() {
+		return errors.New("end_date: cannot be blank")
+	}
+	if s.EndDate.Before(s.StartDate) {
+		return errors.New("end_date: must be after start_date")
+	}
+	return nil
+}
+
+type Response struct {
+	ID          string    `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	Priority    string    `json:"priority"`
+	StartDate   time.Time `json:"start_date"`
+	EndDate     time.Time `json:"end_date"`
+}
+
+func ParseFromEntity(data Entity) (res Response) {
+	res = Response{
+		ID:          data.ID,
+		Title:       *data.Title,
+		Description: *data.Description,
+		Status:      *data.Status,
+		Priority:    *data.Priority,
+		StartDate:   data.StartDate,
+		EndDate:     data.EndDate,
+	}
+	return
+}
+
+func ParseFromEntities(data []Entity) (res []Response) {
+	res = make([]Response, 0)
+	for _, object := range data {
+		res = append(res, ParseFromEntity(object))
+	}
+	return
+}
--- internal/domain/task/entity.go
+++ internal/domain/task/entity.go
@@ -1,5 +1,13 @@
 package task
 
+import "time"
+
 type Entity struct {
-	ID
+	ID          string    `db:"id" bson:"_id"`
+	Title       *string   `db:"title" bson:"title"`
+	Description *string   `db:"description" bson:"description"`
+	Status      *string   `db:"status" bson:"status"`
+	Priority    *string   `db:"priority" bson:"priority"`
+	StartDate   time.Time `db:"start_date" bson:"start_date"`
+	EndDate     time.Time `db:"end_date" bson:"end_date"`
 }
